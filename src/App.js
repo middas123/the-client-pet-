1149caea8b2756d197fe0ba4c365cbb6f999e704
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2024-03-15 21:38:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+// import components
+import Hero from './components/Hero';
+
 const App = () => {
-  return <div>react app</div>;
+  return (
+   <div className='w-full max-w-[1440px]
+    mx-auto bg-orange-quaternary relative'>
+  
+    <Hero />
+   </div>
+  );
 };
 
 export default App;
